28c2578151d5f4bfa72c514a1379c72caf5ab3ce
Joshua Yurtsever <joshua.yurtsever@berkeley.edu>
2020-04-11 01:13:55 -0700

    added requirements and Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
+import logging
+import os
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 
 app = Flask(__name__)
@@ -14,3 +17,22 @@ login.login_view = 'login'
 
 
 from app import routes, models
+
+def create_app(config_class=Config):
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/lipsi.log',
+                                           maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Lipsi startup')
